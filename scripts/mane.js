f9baca5d948f35cfc871bb20fcd00bd8a5a07d62
agentfreedom <agentfreedom@zoho.com>
2021-09-20 19:48:19 +0300

    feat: add a script for detecting mobile versions of the user's gadget with the subsequent addition of css classes

---
+++ scripts/mane.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_tourch');	
+	let menuArrow = document.querySelectorAll('.menu__arrow');
+	if (menuArrow.length > 0) {
+		for (let index = 0; index < menuArrow.length; index++) {
+			const menuArrow = menuArrow[index];
+			menuArrow.addEventListener('click', function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	}
+} else {
+	document.body.classList.add('_pc');
+}
