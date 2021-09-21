36c5efd67e7825e60bdd9568171cad1109ad55c0
agentfreedom <agentfreedom@zoho.com>
2021-09-21 11:09:40 +0300

    feat: add a script for smooth scrolling by classes of sections

--- scripts/mane.js
+++ scripts/mane.js
@@ -29,10 +29,10 @@ const isMobile = {
 if (isMobile.any()) {
 	document.body.classList.add('_touch');
 	
-	let menuArrow = document.querySelectorAll('.menu__arrow');
-	if (menuArrow.length > 0) {
-		for (let index = 0; index < menuArrow.length; index++) {
-			const menuArrow = menuArrow[index];
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
 			menuArrow.addEventListener('click', function (e) {
 				menuArrow.parentElement.classList.toggle('_active');
 			});
@@ -40,4 +40,27 @@ if (isMobile.any()) {
 	}
 } else {
 	document.body.classList.add('_pc');
+}
+
+
+//Плавная прокрутка по клику к секции
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: 'smooth'
+			});
+			e.preventDefault();
+		}
+	}
 }
