d284a119340e2b8497fe54ef8760875b6798658f
agentfreedom <agentfreedom@zoho.com>
2021-09-21 17:29:47 +0300

    refactor: change padding between for validation

--- scripts/mane.js
+++ scripts/mane.js
@@ -31,7 +31,7 @@ if (isMobile.any()) {
 	
 	let menuArrows = document.querySelectorAll('.menu__arrow');
 	if (menuArrows.length > 0) {
-		for (let index = 0; index < menuArrows.length; index++) {
+		for (let index = 0; index<menuArrows.length; index++) {
 			const menuArrow = menuArrows[index];
 			menuArrow.addEventListener('click', function (e) {
 				menuArrow.parentElement.classList.toggle('_active');
